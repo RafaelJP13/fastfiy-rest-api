2ee19f86b002d07cb6909375ebb3df99d54161c3
R <Rafaelsf013@gmail.com>
2021-06-21 13:39:05 -0300

    Add Schemas for rules that will be aplicated at routes options

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,15 +1,55 @@
 const items = require('../../Items')
 
+
+const Item = {
+
+  
+    type:'object',
+    properties:{
+        id: {type:'string'},
+        name: {type:'string'}
+        
+    }
+}
+
+const getItemsOpts = {
+
+    schema:{
+        response:{
+            200:{
+            type:'array',
+            items:Item
+            }
+        }
+    }
+}
+
+
+
+const getItemOpts = {
+
+    schema:{
+    response:{
+        200:Item,
+
+        }
+    }
+
+}
+
+
+
+
 const itemRoutes = (fastify, options, done) =>{
 
-    fastify.get('/items', (req, reply) => {
+    fastify.get('/items', getItemsOpts, (req, reply) => {
 
         reply.send(items)
 
     })
 
 
-    fastify.get('/items/:id', (req, reply) => {
+    fastify.get('/items/:id', getItemOpts, (req, reply) => {
 
         const {id} = req.params
         const item = items.find(item => item.id === id)
